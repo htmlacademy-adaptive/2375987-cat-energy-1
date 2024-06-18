725c1c3266d926761bb87202f7aedba6a96384c4
Nigina Zaripova <zaripovanigina1@gmail.com>
2024-06-19 02:10:16 +0300

    Добавляет иконки

---
+++ source/script.js
@@ -0,0 +1,14 @@
+let navMain = document.querySelector('.page-navigation');
+let navToggle = document.querySelector('.page-navigation__toggle');
+
+navMain.classList.remove('page-navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('page-navigation--closed')) {
+    navMain.classList.remove('page-navigation--closed');
+    navMain.classList.add('page-navigation--opened');
+  } else {
+    navMain.classList.add('page-navigation--closed');
+    navMain.classList.remove('page-navigation--opened');
+  }
+});
